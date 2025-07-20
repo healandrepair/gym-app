e8910250d1baec096bc4c0987d5ab2df7451f8c7
Evan <healandrepair@outlook.com>
2025-07-21 00:09:18 +1200

    deploy

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 1401, hash: '4aae75a13ddcde35d0c23f8495f3c4649d2ad440973e8abac06a57669e97cf0b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1483, hash: '97038f3938b44a0de631b00ab139a4aa58be6f86f80933fed1bac3a4e2e47af9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1401, hash: '6ced831f53d629e63e7b3d7c074293b1eaa75741c4a13cc5c77e94d2c7f03c11', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1483, hash: '20f39e88b46952b996fd2c9873c5bb1dde1cd23f78cce040255b4f2e7e56d825', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-7QHFXQUZ.css': {size: 2253, hash: 'AF2Bys9YQsk', text: () => import('./assets-chunks/styles-7QHFXQUZ_css.mjs').then(m => m.default)}
   },
 };
